1aeacf0c2c8016c6164cf01db1aaf60f8dbcc319
Abel Yoshuara <abelyoshuara@gmail.com>
2024-01-03 22:28:59 +0800

    add middleware in constructor

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:sanctum', ['except' => ['login', 'register']]);
+    // }
+
     public function login(Request $request)
     {
         $request->validate([
